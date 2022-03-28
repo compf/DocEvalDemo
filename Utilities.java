72c8aeab7d64b3ada6181cc2cc318785d14f0daa
tschoemaker <you@example.com>
2022-03-28 20:37:40 +0200

    test commit

--- Utilities.java
+++ Utilities.java
@@ -44,7 +44,7 @@ public class Utilities{
 public void method2(){}
 public void method3(){}
 public void method4(){}
-public void method5(){}
+public void method5(){} 
 public void method6(){}
 public void method7(){}
 public void method8(){}
